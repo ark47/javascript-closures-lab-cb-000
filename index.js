31ca3b66008ac656a75f0e5aa5afa4205e93e511
Chris Nunez <cenunez47@gmail.com>
2018-04-08 15:08:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 const app = "I don't do much."
+
+function bumpCounter(callback) {
+  var counter = 0
+
+  function addBump() {
+    return counter++;
+  }
+
+  function getBumps() {
+    return counter;
+  }
+
+  return {
+    addBump,
+    getBumps
+  };
+}
+
+function createAnimal(animalType) {
+  function deadlyDevice(weapon) {}
+}
